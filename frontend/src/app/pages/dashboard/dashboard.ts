1562caa4d082522246b2f8a03d52421a478c990c
Erik Souza <erikgsouza.dev@gmail.com>
2026-05-19 18:17:17 -0300

    feat: dashboard inicial com sidebar e topbar

--- frontend/src/app/pages/dashboard/dashboard.ts
+++ frontend/src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Sidebar } from '../../components/sidebar/sidebar';
+import { Topbar } from '../../components/topbar/topbar';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterLink, Sidebar, Topbar],
   templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+  styleUrl: './dashboard.css'
 })
-export class Dashboard {}
+export class Dashboard {
+
+}
